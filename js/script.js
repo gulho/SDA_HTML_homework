827860337a84c7420301f1e5529ad94a51ee9a34
gulho <gulhos@gmail.com>
2019-09-30 20:53:07 +0300

    stage2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-$('#userForm').submit(function () {
-    var firstName = $('#firstName').val();
-    var password = $('#password').val();
+$('#userForm').submit(function (e) {
+    e.preventDefault();
+    var firstName = $('#firstName').val().trim();
+    var password = $('#password').val().trim();
     var gender = $('#gender').val();
-    var address = $('#address').val();
+    var address = $('#address').val().trim();
     var marital = $('#marital').val();
     var games = [];
     var agreement = $('#agreement').is(':checked');
 
     $('.games:checked').each(function() {
-        games += games.add(this.val());
-    })
+        games.push($(this).val());
+    });
 
-    console.log(games);
+
+    $('#errors').empty();
+    if(firstName == "") {
+        showError("First name is mandatory")
+    }
+    if(password == "") {
+        showError("Password is mandatory")
+    }
+    if(address == "") {
+        showError("Address is mandatory")
+    }
+    if(!agreement) {
+        showError("Agreement must be checked");
+    }
     return false;
 });
+
+function showError(str) {
+    var alert = $('<div>').addClass("alert alert-danger").attr("role", "alert").text(str)
+    $('#errors').append(alert);
+}

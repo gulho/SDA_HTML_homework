57b84b92eb95d6f59690ee3ae24ebbdebbfcc600
gulho <gulhos@gmail.com>
2019-09-30 21:40:16 +0300

    stage3

--- js/script.js
+++ js/script.js
@@ -4,27 +4,48 @@ $('#userForm').submit(function (e) {
     var password = $('#password').val().trim();
     var gender = $('#gender').val();
     var address = $('#address').val().trim();
+    var dayOfBirth = $('#dayOfBirth').val();
     var marital = $('#marital').val();
     var games = [];
     var agreement = $('#agreement').is(':checked');
-
-    $('.games:checked').each(function() {
+    var errors = $('#errors');
+    errors.empty();
+    $('.games:checked').each(function () {
         games.push($(this).val());
     });
 
-
-    $('#errors').empty();
-    if(firstName == "") {
-        showError("First name is mandatory")
+    //$('#errors').empty();
+    if (firstName === "") {
+        showError("First name is mandatory");
     }
-    if(password == "") {
-        showError("Password is mandatory")
+    if (password === "") {
+        showError("Password is mandatory");
     }
-    if(address == "") {
-        showError("Address is mandatory")
+    if (address === "") {
+        showError("Address is mandatory");
     }
-    if(!agreement) {
+    if (dayOfBirth === "") {
+        showError("Day of birth is mandatory");
+    }
+    if (!agreement) {
         showError("Agreement must be checked");
+    }
+    if (errors.children().length === 0) {
+        $.post("https://5d7e077fd756030014184069.mockapi.io/user",
+            {
+                "firstName": firstName,
+                "password": password,
+                "gender": gender,
+                "address": address,
+                "dob": dayOfBirth,
+                "games": games,
+                "maritalStatus": marital,
+                "acceptAgreement": agreement
+            },
+            function (response) {
+                console.log(response)
+            }
+        );
     }
     return false;
 });

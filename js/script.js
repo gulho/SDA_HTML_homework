1a9ac5c587a220b5f3f8da53224bd6eb813b5db2
klyappe <gulhos@gmail.com>
2019-09-30 17:43:54 +0300

    stage1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-$(document).ready(function () {
-    $('#userForm').submit(function () {
-        console.log('da');
-        return false;
-    });
+$('#userForm').submit(function () {
+    var firstName = $('#firstName').val();
+    var password = $('#password').val();
+    var gender = $('#gender').val();
+    var address = $('#address').val();
+    var marital = $('#marital').val();
+    var games = [];
+    var agreement = $('#agreement').is(':checked');
+
+    $('.games:checked').each(function() {
+        games += games.add(this.val());
+    })
+
+    console.log(games);
+    return false;
 });
